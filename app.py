6ebd3a175270287c89b545c8a8c4d671787ec231
Luan fonseca de farias <luanfonceca@gmail.com>
2012-08-29 21:35:49 -0300

    Adicionando procfile e foreman

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
